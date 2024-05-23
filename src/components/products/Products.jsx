6a9dcc168e1a3f54ee4862a5cddc55a46cf6c53a
djboburjon <djboburdev@gmail.com>
2024-05-23 12:17:07 -0700

    getProduct function added

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Products.css";
 import { Link } from "react-router-dom";
 import { TiPlus } from "react-icons/ti";
+import { FaEdit, FaSearch } from "react-icons/fa";
 
 function Products({ token }) {
+  const [products, setProducts] = useState([]);
+
+  const getProduct = () => {
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${token}`);
+
+    const requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://telzone.pythonanywhere.com/product/all/?status=on_sale",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setProducts(result))
+      .catch((error) => console.error(error));
+  };
+
+  useEffect(() => {
+    getProduct();
+  }, [token]);
   return (
     <div className="productSection">
       <div className="container">
@@ -63,13 +88,83 @@ function Products({ token }) {
         <div className="main_right">
           <div className="main_right-head">
             <h3>O'zgartirish uchun mahsulot tanlang</h3>
-            <button
-              className="client_add"
-              
-            >
-              MAHSULOT
+            <button className="client_add">
+              MAHSULOT QO'SHISH
               <TiPlus />
             </button>
+          </div>
+          <div className="client_search">
+            <form action="">
+              <FaSearch />
+              <input type="text" placeholder="Qidiruv..." />
+            </form>
+          </div>
+          <table className="client_table">
+            <thead>
+              <tr>
+                <th>№</th>
+                <th>Nomi</th>
+                <th>Olingan Narx</th>
+                <th>Foiz</th>
+                <th>Sotish Narxi</th>
+                <th>Soni</th>
+                <th>Sana</th>
+                <th>Holati</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products?.results?.slice(0, 25).map((item, index) => {
+                return (
+                  <tr key={item.id}>
+                    <td>{index + 1}</td>
+                    <td>{item.name}</td>
+                    <td>{item.purchase_price}</td>
+                    <td>{item.percent}</td>
+                    <td>{item.price}</td>
+                    <td>{item.count}</td>
+                    <td>{item.date}</td>
+                    <td>{item.status}</td>
+                    <td className="editClient_btn">
+                      <FaEdit />
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <div className="client_count">{products?.count} ta mijoz</div>
+
+          <div className="paginations">
+            <div className="prev">
+              {products.previous ? (
+                <button
+                  onClick={() => {
+                    prevData();
+                  }}
+                >
+                  Ortga
+                </button>
+              ) : (
+                <button className="disabled_btn" disabled>
+                  Ortga
+                </button>
+              )}
+            </div>
+            <div className="next">
+              {products.next ? (
+                <button
+                  onClick={() => {
+                    nextData();
+                  }}
+                >
+                  Keyingi
+                </button>
+              ) : (
+                <button className="disabled_btn" disabled>
+                  Keyingi
+                </button>
+              )}
+            </div>
           </div>
         </div>
       </div>
