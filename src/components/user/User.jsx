45de523800a59e9d33a847c834a70eb3c498325f
djboburjon <djboburdev@gmail.com>
2024-06-20 13:49:17 -0700

    adminOrWorker toggle added

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -4,17 +4,23 @@ import { Link } from "react-router-dom";
 import { FaEdit, FaSearch } from "react-icons/fa";
 import EditUser from "../editUser/EditUser";
 import { TiPlus } from "react-icons/ti";
+import Switch from "@mui/material/Switch";
 
 function User({ token }) {
-  const [user, setUser] = useState();
+  const [user, setUser] = useState([]);
   const [addUser, setAddUser] = useState(false);
   const [editUser, setEditUser] = useState(false);
   const [editUsername, setEditUsername] = useState("");
   const [editName, setEditName] = useState("");
   const [editLastName, setEditLastName] = useState("");
   const [editUserpassword, setEditUserpassword] = useState("");
+  const [editUserRole, setEditUserRole] = useState("");
   const [itemId, setItemId] = useState(0);
   const [changed, setChanged] = useState(false);
+  const [userType, setUserType] = useState("admin");
+  const [didHanSoloShootFirst, setDidHanSoloShootFirst] = useState(true);
+
+  const label = { inputProps: { "aria-label": "Color switch demo" } };
 
   const getUser = () => {
     const myHeaders = new Headers();
@@ -27,13 +33,25 @@ function User({ token }) {
     };
 
     fetch(
-      "https://telzone.pythonanywhere.com/user/all/?enum=admin",
+      `https://telzone.pythonanywhere.com/user/all/?enum=${userType}`,
       requestOptions
     )
       .then((response) => response.json())
       .then((result) => setUser(result))
       .catch((error) => console.error(error));
   };
+
+  function toggleDidHanSoloShootFirst() {
+    if (didHanSoloShootFirst === true) {
+      setDidHanSoloShootFirst(false);
+      setUserType("worker");
+      setChanged(!changed);
+    } else if (didHanSoloShootFirst === false) {
+      setDidHanSoloShootFirst(true);
+      setUserType("admin");
+      setChanged(!changed);
+    }
+  }
 
   useEffect(() => {
     getUser();
@@ -55,6 +73,7 @@ function User({ token }) {
         setEditUsername(result.username);
         setEditName(result.first_name);
         setEditLastName(result.last_name);
+        setEditUserRole(result.role);
       })
       .catch((error) => console.error(error));
   };
@@ -91,6 +110,8 @@ function User({ token }) {
           {editUser && (
             <EditUser
               token={token}
+              user={user}
+              setUser={setUser}
               editUser={editUser}
               setEditUser={setEditUser}
               changed={changed}
@@ -103,6 +124,8 @@ function User({ token }) {
               setEditLastName={setEditLastName}
               editUserpassword={editUserpassword}
               setEditUserpassword={setEditUserpassword}
+              editUserRole={editUserRole}
+              setEditUserRole={setEditUserRole}
               itemId={itemId}
             />
           )}
@@ -128,6 +151,17 @@ function User({ token }) {
               <FaSearch />
               <input type="text" placeholder="Qidiruv..." />
             </form>
+          </div>
+
+          <div className="adminOrWorker">
+            <span>Ishchilar</span>
+            <Switch
+              {...label}
+              onClick={toggleDidHanSoloShootFirst}
+              defaultChecked
+              color="default"
+            />{" "}
+            <span>Adminlar</span>
           </div>
 
           <table className="client_table">
